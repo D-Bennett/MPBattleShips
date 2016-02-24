466509f4d45721583e77faffbcfdc74210063623
Dave <dmbennett92@gmail.com>
2016-02-24 11:52:09 +0000

    Moved ship placement to the server

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var MPMono_Server = function() {
 	var _self = this;
 	_self.game_state = 0; //0=Waiting, 1=Setup, 2=Runing, 3=Gameover
 	_self.players = [null,null];
-	_self.players_data = [{grid:null,attacks:null,ships:null},{grid:null,attacks:null,ships:null}];
+	_self.players_data = [
+		{grid:null,attacks:null,ships:[{x:0,y:0,d:1,s:1},{x:0,y:1,d:1,s:2},{x:0,y:2,d:1,s:3},{x:0,y:3,d:1,s:4},{x:0,y:4,d:1,s:5}]},
+		{grid:null,attacks:null,ships:[{x:0,y:0,d:1,s:1},{x:0,y:1,d:1,s:2},{x:0,y:2,d:1,s:3},{x:0,y:3,d:1,s:4},{x:0,y:4,d:1,s:5}]}
+	];
 	_self.players_placed = [0,0];
 	_self.players_ready = [0,0];
 	_self.turn = 0;
@@ -49,7 +52,7 @@ var MPMono_Server = function() {
 				if (_self.players_placed[player_slot] == 1) {
 					_self.players_ready[player_slot] = 1;
 					if (_self.players_ready[0] == 1 && _self.players_ready[1] == 1) {
-						_self.game_run();
+						_self.set_game_state(2);
 					} else {
 						_self.send_game_data();
 					}
@@ -57,11 +60,14 @@ var MPMono_Server = function() {
 			}
 		}
 	}
-	_self.game_run = function () {
-		if (_self.game_state == 0) {
+	_self.set_game_state = function (ns) {
+		if (ns == 0) {
+			_self.game_state = 0;
+			
+		} else if (ns == 1) {
 			_self.game_state = 1;
 			
-		} else if (_self.game_state == 1) {
+		} else if (ns == 2) {
 			_self.game_state = 2;
 
 		}
@@ -73,15 +79,44 @@ var MPMono_Server = function() {
 	}
 	_self.send_game_data = function() {
 		for (var p=0; p<_self.players.length; p++) {
-			if (_self.players[p] !== null) {
-				var blob = {
-					game_state: _self.game_state,
-					turn: _self.turn,
-					data: _self.players_data[p],
-				};
-				_self.players[p].emit('game-data', blob);
+			_self.send_player_game_data(p);
+		}
+	}
+	_self.send_player_game_data = function(p) {
+		if (_self.players[p] !== null) {
+			var blob = {
+				game_state: _self.game_state,
+				turn: _self.turn,
+				data: _self.players_data[p],
+			};
+			_self.players[p].emit('game-data', blob);
+		}
+	}
+	_self.ship_to_blocks = function (ship) {
+		var points = [];
+		for (var i=0; i<ship.s; i++) {
+			points.push([ship.x + (ship.d==1?i:0), ship.y + (ship.d==0?i:0)]);
+		}
+		return points;
+	}
+	_self.valid_ship_placement = function (ship, ships, newpos) {
+		var ship_points = _self.ship_to_blocks({x:newpos.x, y:newpos.y, d:newpos.d, s:ships[ship].s});
+		for (var p=0; p<ship_points.length; p++) {
+			if (ship_points[p][0]<0||ship_points[p][1]<0||ship_points[p][0]>9||ship_points[p][1]>9) {
+				return false;
+			}
+			for (var q=0; q<ships.length; q++) {
+				if (q != ship) {
+					var other_ship_points = _self.ship_to_blocks(ships[q]);
+					for (var r=0; r<other_ship_points.length; r++) {
+						if (other_ship_points[r][0] == ship_points[p][0] && other_ship_points[r][1] == ship_points[p][1]) {
+							return false;
+						}
+					}
+				}
 			}
 		}
+		return true;
 	}
 	_self.init = function() {
 		for (var p=0; p<_self.players.length; p++) {
@@ -99,9 +134,26 @@ var MPMono_Server = function() {
 			_self.send_game_data();
 			if (_self.game_state == 0) {
 				if (_self.players[0] !== null && _self.players[1] !== null) {
-					_self.game_run();
+					_self.set_game_state(1);
 				}
 			}
+			socket.on('place-ship', function(data){
+				console.log("place-ship", data);
+
+				var ship = data.index;
+				if (_self.valid_ship_placement(ship, _self.players_data[slot].ships, data.data)) {
+					console.log("A",_self.players_data[slot].ships[ship])
+					_self.players_data[slot].ships[ship].x = data.data.x;
+					_self.players_data[slot].ships[ship].y = data.data.y;
+					_self.players_data[slot].ships[ship].d = data.data.d;
+					console.log("B",_self.players_data[slot].ships[ship]);
+					_self.send_player_game_data(slot);
+				}
+
+			});
+			socket.on('ready', function(){
+				console.log("READY");
+			});
 			socket.on('disconnect', function(){
 				_self.player_disconnect(slot);
 				_self.io.emit('player-left', slot);
